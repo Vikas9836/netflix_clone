403fa0efb18acbde7acb8a0f68b5e499c518a3f0
Vikas9836 <118796479+Vikas9836@users.noreply.github.com>
2023-01-28 00:45:14 +0530

    file added

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+import React from 'react'
+import Home from './page/Home';
+
+
+function App (){
+  return (
+    <div className='cont'>
+     <Home></Home>
+    </div>
+  )
+}
+
+export default App
+
